1a79bade6c1dd765e750ec97c58f265efd758f46
李亚军 <dreamoflu@163.com>
2021-08-31 23:14:05 +0800

    chore: json-serve 登录接口

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
 import React from "react";
 // import logo from "logo.svg";
 import "./App.css";
-import { ProjectListScreen } from "./screens/project-list";
+// import { ProjectListScreen } from "./screens/project-list";
 
-import { TsReactTest } from "./try-use-array";
+// import { TsReactTest } from "./try-use-array";
+import { LoginScreen } from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest />
-      <ProjectListScreen />
-
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
+      {/*<TsReactTest />*/}
+      {/*<ProjectListScreen />*/}
+      <LoginScreen />
     </div>
   );
 }
