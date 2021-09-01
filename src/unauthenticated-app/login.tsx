a4a55e3cbedd03ce42697eb92d65e0cd9794584a
李亚军 <dreamoflu@163.com>
2021-09-02 01:13:26 +0800

    feat: 安装emotion，优化登录注册界面

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,29 +1,46 @@
-import { FormEvent } from "react";
+// import { FormEvent } from "react";
 import { useAuth } from "../context/auth-context";
+import { Button, Form, Input } from "antd";
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
   // HTMLFormElement extends Element
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    login({ username, password });
+  // 未使用antd
+  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const username = (event.currentTarget.elements[0] as HTMLInputElement)
+  //     .value;
+  //   const password = (event.currentTarget.elements[1] as HTMLInputElement)
+  //     .value;
+  //   login({ username, password });
+  // };
+
+  // 使用antd
+  const handleSubmit = (values: { username: string; password: string }) => {
+    login(values);
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onFinish={handleSubmit}>
       {user ? <div>登录成功，用户名：{user?.name}</div> : null}
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id={"username"} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id={"password"} />
-      </div>
-      <button type={"submit"}>登录</button>
-    </form>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        {/*<label htmlFor="username">用户名</label>*/}
+        <Input placeholder={"用户名"} type="text" id={"username"} />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        {/*<label htmlFor="password">密码</label>*/}
+        <Input placeholder={"密码"} type="password" id={"password"} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType={"submit"} type={"primary"}>
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
