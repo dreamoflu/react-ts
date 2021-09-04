08a064dd14a6e1acc5518820c192f1e126a6ae93
李亚军 <dreamoflu@163.com>
2021-09-04 16:16:45 +0800

    feat: kanban和epic路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -9,6 +9,7 @@ import { Button, Dropdown, Menu } from "antd";
 import { Navigate, Route, Routes } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ProjectScreen } from "./screens/project";
+import { resetRoute } from "./utils";
 
 /**
  * grid 和 flex 各自的应用场景
@@ -35,6 +36,7 @@ export const AuthenticatedApp = () => {
               path={"/projects/:projectId/*"}
               element={<ProjectScreen />}
             />
+            <Navigate to={"/projects"} />
           </Routes>
         </Router>
       </Main>
@@ -46,7 +48,10 @@ const PageHeader = () => {
   return (
     <Header between={true}>
       <HeaderLeft gap={true}>
-        <SoftwareLogo width={"18rem"} color={"reg(38,132,255)"} />
+        <Button type={"link"} onClick={resetRoute}>
+          <SoftwareLogo width={"18rem"} color={"reg(38,132,255)"} />
+        </Button>
+
         <h2>项目</h2>
         <h2>用户</h2>
       </HeaderLeft>
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router";
+import { KanbanScreen } from "../Kanban";
+import { EpicScreen } from "../epic";
+
 export const ProjectScreen = () => {
-  return <h1>11111</h1>;
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={"kanban"}>看板</Link>
+      <Link to={"epic"}>任务组</Link>
+      <Routes>
+        <Route path={"kanban"} element={<KanbanScreen />}></Route>
+        <Route path={"epic"} element={<EpicScreen />} />
+        <Navigate to={"kanban"} />
+      </Routes>
+    </div>
+  );
 };
